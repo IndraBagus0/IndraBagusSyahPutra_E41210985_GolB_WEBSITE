6649a6af07ff5e57e3c87e4c0443a15546de59e6
Indra_Bagus_GolB_mobile <indrabusiness00@gmail.com>
2023-05-02 11:29:13 +0700

    CRUD Query builder

--- MINGGU 7/database/seeders/DatabaseSeeder.php
+++ MINGGU 7/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\PengalamanKerja;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //\App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        PengalamanKerja::factory(30)->create();
+        $this->call([
+            EmployeSeeder::class
+        ]);
     }
 }
